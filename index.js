febed7cd8808da42a7bed248cdd6894afce2b3ac
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2020-09-03 12:58:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var rect = {
+    perimeter:(x,y)=>(2*(x+y)),
+    area:(x,y)=>(x*y)
+}
+
+function solveRect(l,b){
+    console.log('solving for rectange with l=' + l + ' and b='+ b)
+    if(l<=0 || b <=0){
+        console.log('rectange dimensions should be greater than zero: l=' + l + ', b=' + b)
+
+    }
+    else{
+        console.log("The area of the rectange is: " + rect.area(l,b))
+        console.log("The perimeter of the rectangle is: " + rect.perimeter(l,b))
+    }
+}
+
+
+solveRect(2,4);
+solveRect(0,4);
+solveRect(3,5);
+solveRect(-3,5);
